37b4a3c10757ab69322afe04ecc6c0eefafa6333
sidinfinity <sidinfinity0@gmail.com>
2023-08-10 20:17:54 -0700

    Updated the model.py file, and created a new test.py file

--- chemNLP.py
+++ chemNLP.py
@@ -7,12 +7,14 @@ import torch.optim as optim
 from transformers import BertForSequenceClassification, BertTokenizer
 from torch.utils.data import TensorDataset, DataLoader
 import numpy as np
+import os
 
 
 def main():
-    csv_path = '/Users/siddharthmaddikayala/machine_learning/data/tox21.csv'
+    csv_path = os.path.join(os.getcwd(), 'data', 'HIV.csv')
     df = pd.read_csv(csv_path)
-    del df['mol_id'] # removing the name of the molecule since that doesn't matter
+    print("Data Loaded")
+    del df['activity'] # removing the name of the molecule since that doesn't matter
     df.replace({'true': 1, 'false': 0}, inplace=True)
     df.fillna(-1, inplace=True) #changing "Nan" to -1
 
@@ -27,8 +29,11 @@ def main():
 
 
     train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
+    print("Train Test Split Finished")
+
     model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels = num_classes)
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    print("Model and Tokenizer Initialized")
 
     # Tokenize and preprocess data
     encoded_data_train = tokenizer.batch_encode_plus(
@@ -43,100 +48,59 @@ def main():
     input_ids_train = encoded_data_train['input_ids']
     attention_masks_train = encoded_data_train['attention_mask']
     labels_train = torch.tensor(train_df.drop(columns=['smiles']).values, dtype=torch.float)
+    print("Data tokenization and preprocessing complete")
 
     # Create DataLoader
     train_dataset = TensorDataset(input_ids_train, attention_masks_train, labels_train)
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    print("Train DataLoader created")
 
     # Define optimizer and loss function
     optimizer = optim.AdamW(model.parameters(), lr=learning_rate)
-    criterion = torch.nn.BCEWithLogitsLoss()
+    criterion = torch.nn.CrossEntropyLoss()
+    label_columns = list(df.columns)
+    label_columns.remove("smiles")
 
+    label_accuracies = {label: 0.0 for label in label_columns}
+    label_sample_counts = {label: 0 for label in label_columns}
+
+    """
+    Cross Entropy Loss instead of BCE
+    Calculate Loss for each column inside a loop
+    """
     # Training loop
+
+    print("STARTING TRAINING")
     model.train()
     for epoch in range(num_epochs):
-        total_loss = 0.0
-        total_correct = 0
         total_samples = 0
         for batch_input_ids, batch_attention_masks, batch_labels in train_loader:
             optimizer.zero_grad()
             outputs = model(batch_input_ids, attention_mask=batch_attention_masks)
-            loss = criterion(outputs.logits, batch_labels)
+            predicted_labels = outputs.logits.argmax(dim=1)
+            total_loss = 0.0
+
+            for label_idx, label in enumerate(label_columns):
+                correct_labels = (predicted_labels == batch_labels[:, label_idx]).sum().item() #calculates the sum of "true" in the created tensor
+                label_accuracies[label] += correct_labels
+                label_sample_counts[label] += len(batch_labels)
+
+                column_predictions = outputs.logits[:, label_idx]  # Predictions for this column
+                column_labels = batch_labels[:, label_idx]  # True labels for this column
+                loss = criterion(column_predictions, column_labels)
+                total_loss += loss
+
+
+            print(f"Epoch {epoch+1}/{num_epochs} - Batch Loss: {total_loss:.4f}")
             loss.backward()
             optimizer.step()
-
-            total_loss += loss.item()
-
-            # Calculate accuracy
-            predicted_labels = (outputs.logits > 0.5).float()  # Threshold logits for binary classification
-            batch_correct = (predicted_labels == batch_labels).all(dim=1).sum().item()
-            total_correct += batch_correct
-            total_samples += len(batch_labels)
         
-        average_loss = total_loss / len(train_loader)
-        accuracy = total_correct / total_samples
-
-        print(f"Epoch {epoch+1}/{num_epochs} - Train Loss: {average_loss:.4f} - Train Accuracy: {accuracy:.4f}")
+        for label in label_columns:
+            label_accuracy = label_accuracies[label] / label_sample_counts[label]
+            print(f"Accuracy for {label}: {label_accuracy:.4f}")
 
     # Example SMILES string
-    torch.save(model.state_dict(), "/Users/siddharthmaddikayala/machine_learning/models/tox21.pth")
-
-def test():
-    df = pd.read_csv('/Users/siddharthmaddikayala/machine_learning/data/tox21.csv')
-
-    del df['mol_id']
-    df.replace({'true': 1, 'false': 0}, inplace=True)
-    df.fillna(-1, inplace=True)
-
-    # Define constants
-    num_classes = len(df.columns) - 1  # Number of labels excluding the text column
-    max_sequence_length = df['smiles'].apply(len).max()
-    batch_size = 32 
-
-    # Split the data into train and test sets
-    train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
-
-    # Initialize BERT model and tokenizer
-    model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=num_classes)
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-
-    # Tokenize and preprocess test data
-    encoded_data_test = tokenizer.batch_encode_plus(
-        test_df['smiles'].tolist(),
-        add_special_tokens=True,
-        padding=True,
-        return_attention_mask=True,
-        max_length=max_sequence_length,
-        return_tensors='pt',
-        truncation=True
-    )
-    input_ids_test = encoded_data_test['input_ids']
-    attention_masks_test = encoded_data_test['attention_mask']
-    labels_test = torch.tensor(test_df.drop(columns=['smiles']).values, dtype=torch.float)
-
-    # Create DataLoader for testing
-    test_dataset = TensorDataset(input_ids_test, attention_masks_test, labels_test)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-
-    # Load the saved model
-    model.load_state_dict(torch.load('/Users/siddharthmaddikayala/machine_learning/models/tox21.pth'))
-    model.eval()
-
-    # Test the model
-    correct_predictions = 0
-    total_samples = 0
-
-    with torch.no_grad():
-        for batch_input_ids, batch_attention_masks, batch_labels in test_loader:
-            outputs = model(batch_input_ids, attention_mask=batch_attention_masks)
-            predicted_labels = (outputs.logits > 0.5).float()  # Threshold logits for binary classification
-            correct_predictions += (predicted_labels == batch_labels).all(dim=1).sum().item()
-            total_samples += len(batch_labels)
-
-    accuracy = correct_predictions / total_samples
-    print(f"Test Accuracy: {accuracy:.4f}")
-   
-
+    torch.save(model.state_dict(), os.path.join(os.getcwd(), 'models', 'HIV.pth'))
 
 if __name__ == "__main__":
     main()
---
+++ test.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import torch
+import torch.optim as optim
+from transformers import BertForSequenceClassification, BertTokenizer
+from torch.utils.data import TensorDataset, DataLoader
+import numpy as np
+import os
+
+def main():
+    df = pd.read_csv(os.path.join(os.getcwd(), "data", "tox21.csv"))
+
+    del df['mol_id']
+    df.replace({'true': 1, 'false': 0}, inplace=True)
+    df.fillna(-1, inplace=True)
+
+    # Define constants
+    num_classes = len(df.columns) - 1  # Number of labels excluding the text column
+    max_sequence_length = df['smiles'].apply(len).max()
+    batch_size = 32 
+
+    # Split the data into train and test sets
+    train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
+
+    # Initialize BERT model and tokenizer
+    model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=num_classes)
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+    # Tokenize and preprocess test data
+    encoded_data_test = tokenizer.batch_encode_plus(
+        test_df['smiles'].tolist(),
+        add_special_tokens=True,
+        padding=True,
+        return_attention_mask=True,
+        max_length=max_sequence_length,
+        return_tensors='pt',
+        truncation=True
+    )
+    input_ids_test = encoded_data_test['input_ids']
+    attention_masks_test = encoded_data_test['attention_mask']
+    labels_test = torch.tensor(test_df.drop(columns=['smiles']).values, dtype=torch.float)
+
+    # Create DataLoader for testing
+    test_dataset = TensorDataset(input_ids_test, attention_masks_test, labels_test)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    # Load the saved model
+    model.load_state_dict(torch.load(os.path.join(os.getcwd(), "models", "tox21.pth")))
+    model.eval()
+
+    # Test the model
+    label_columns = list(df.columns)
+    label_columns.remove("smiles")
+
+    label_accuracies = {label: 0.0 for label in label_columns}
+    label_sample_counts = {label: 0 for label in label_columns}
+
+    with torch.no_grad():
+        for batch_input_ids, batch_attention_masks, batch_labels in test_loader:
+            outputs = model(batch_input_ids, attention_mask=batch_attention_masks)
+            predicted_labels = (outputs.logits > 0.5).float()  # Threshold logits for binary classification
+
+            for label_idx, label in enumerate(label_columns):
+                label_accuracy = (predicted_labels[:, label_idx] == batch_labels[:, label_idx]).sum().item()
+                label_accuracies[label] += label_accuracy
+                label_sample_counts[label] += len(batch_labels)
+
+    for label in label_columns:
+        label_accuracy = label_accuracies[label] / label_sample_counts[label]
+        print(f"Accuracy for {label}: {label_accuracy:.4f}")
+
+if __name__ == "__main__":
+    main()
